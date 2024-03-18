889af955fcad5202f5daa649d3b5f0239366c97d
민병욱 <vvxxxxk@gmail.com>
2024-03-19 00:20:29 +0900

    jasypt 설정파일 및 테스트 코드 추가

---
+++ src/test/java/com/kream/chouxkream/config/JasyptConfigTest.java
@@ -0,0 +1,31 @@
+package com.kream.chouxkream.config;
+
+import org.assertj.core.api.Assertions;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JasyptConfigTest {
+
+    @Test
+    public void stringEncryptor() {
+
+        String encrptKey = System.getProperty("jasypt.encryptor.password");
+        String plainText = "test";
+
+        StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+        jasypt.setPassword(encrptKey);
+
+        String encryptedText = jasypt.encrypt(plainText);
+        String decryptedText = jasypt.decrypt(encryptedText);
+
+        System.out.println("encryptedText = " + encryptedText);
+        System.out.println("decryptedText = " + decryptedText);
+
+        Assertions.assertThat(plainText).isEqualTo(decryptedText);
+    }
+
+}
