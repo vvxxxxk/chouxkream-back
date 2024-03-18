aee9eb91c88c4a9c8766ca3023ef09fb53318955
민병욱 <vvxxxxk@gmail.com>
2024-03-18 23:39:02 +0900

    project init

---
+++ src/main/java/com/kream/chouxkream/ChouxkreamApplication.java
@@ -0,0 +1,13 @@
+package com.kream.chouxkream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChouxkreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChouxkreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kream/chouxkream/ChouxkreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kream.chouxkream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChouxkreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
