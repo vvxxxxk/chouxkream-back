b913eeb2f0745eea24498e58a17bbbcea11fbefe
민병욱 <vvxxxxk@gmail.com>
2024-03-21 20:20:18 +0900

    example source

---
+++ src/main/java/com/kream/chouxkream/user/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.kream.chouxkream.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/example")
+public class UserController {
+}
