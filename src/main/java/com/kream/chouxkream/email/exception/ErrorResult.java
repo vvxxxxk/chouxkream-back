182796162c94db5dbbd721392148aca42f90b348
gamong <shhwang0930@gmail.com>
2024-03-23 15:45:46 +0900

    email 인증 구현

---
+++ src/main/java/com/kream/chouxkream/email/exception/ErrorResult.java
@@ -0,0 +1,11 @@
+package com.kream.chouxkream.email.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+    private String code;
+    private String message;
+}
