f6d6c068330839fe5285b004222f43bd1d886ad5
jangyc <xzdi0414@gmail.com>
2024-04-06 15:57:52 +0900

    test disabled

--- src/test/java/com/kream/chouxkream/ChouxkreamApplicationTests.java
+++ src/test/java/com/kream/chouxkream/ChouxkreamApplicationTests.java
@@ -2,12 +2,13 @@ package com.kream.chouxkream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
 class ChouxkreamApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
--- src/test/java/com/kream/chouxkream/common/config/JasyptConfigTest.java
+++ src/test/java/com/kream/chouxkream/common/config/JasyptConfigTest.java
@@ -2,27 +2,28 @@ package com.kream.chouxkream.common.config;
 
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Profile;
 
 import static org.assertj.core.api.Assertions.*;
 
 class JasyptConfigTest {
 
-    @Test
-    public void stringEncryptor() {
-
-        String encrptKey = System.getProperty("jasypt.encryptor.password");
-        // String plainText = "jdbc:mysql://localhost:3306/chouxkream_db?UTC&characterEncoding=UTF-8";
-        String plainText = "3hQWN6zPEf";
-
-        StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
-        jasypt.setPassword(encrptKey);
-
-        String encryptedText = jasypt.encrypt(plainText);
-        String decryptedText = jasypt.decrypt(encryptedText);
-
-        System.out.println("encryptedText = " + encryptedText);
-        System.out.println("decryptedText = " + decryptedText);
-
-        assertThat(plainText).isEqualTo(decryptedText);
-    }
+//    @Test
+//    public void stringEncryptor() {
+//
+//        String encrptKey = System.getProperty("jasypt.encryptor.password");
+//        // String plainText = "jdbc:mysql://localhost:3306/chouxkream_db?UTC&characterEncoding=UTF-8";
+//        String plainText = "3hQWN6zPEf";
+//
+//        StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+//        jasypt.setPassword(encrptKey);
+//
+//        String encryptedText = jasypt.encrypt(plainText);
+//        String decryptedText = jasypt.decrypt(encryptedText);
+//
+//        System.out.println("encryptedText = " + encryptedText);
+//        System.out.println("decryptedText = " + decryptedText);
+//
+//        assertThat(plainText).isEqualTo(decryptedText);
+//    }
 }
