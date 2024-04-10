897bbb4a12004f16dee9fd552f753f6da578313f
민병욱 <vvxxxxk@gmail.com>
2024-04-10 17:53:44 +0900

    entity 설계

---
+++ src/main/java/com/kream/chouxkream/brand/model/entity/Brand.java
@@ -0,0 +1,45 @@
+package com.kream.chouxkream.brand.model.entity;
+
+import com.kream.chouxkream.product.model.entity.Product;
+import com.kream.chouxkream.user.model.entity.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Brand {
+
+    @Id
+    private String brandId;
+
+    @Column(nullable = false)
+    private String brandName;
+
+    @Column(nullable = true)
+    private String brandSubName;
+
+    @Column(nullable = true)
+    private String brandLogoUrl;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp createDate;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Timestamp updateDate;
+
+    @Column(nullable = false, columnDefinition = "BIT DEFAULT 0")
+    private boolean isActive;
+
+    @OneToMany(mappedBy = "brand", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Set<Product> products = new HashSet<>();
+}
---
+++ src/main/java/com/kream/chouxkream/product/model/entity/Product.java
@@ -0,0 +1,77 @@
+package com.kream.chouxkream.product.model.entity;
+
+import com.kream.chouxkream.brand.model.entity.Brand;
+import com.kream.chouxkream.category.model.entity.Category;
+import com.kream.chouxkream.productimages.model.entity.ProductImages;
+import com.kream.chouxkream.productsize.model.entity.ProductSize;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productNo;
+
+    @Column(nullable = false)
+    private String productTitle;
+
+    @Column(nullable = false)
+    private String productSubTitle;
+
+    @Column(nullable = true)
+    private String modelId;
+
+    @Column(nullable = true)
+    private String productColor;
+
+    @Column(nullable = true)
+    private Integer releasePrice;
+
+    @Column(nullable = true)
+    private Timestamp releaseDate;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp createDate;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Timestamp updateDate;
+
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int viewsCount;
+
+    @Column(nullable = true)
+    private String metaKeyword;
+
+    @Column(nullable = true)
+    private String metaDesc;
+
+    @Column(nullable = false, columnDefinition = "BIT DEFAULT 0")
+    private boolean isActive;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    @ManyToOne
+    @JoinColumn(name = "category_no")
+    private Category category;
+
+    @OneToMany(mappedBy = "product", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Set<ProductImages> productImages = new HashSet<>();
+
+    @OneToMany(mappedBy = "product", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Set<ProductSize> productSizes = new HashSet<>();
+}
